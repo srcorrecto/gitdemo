e272fbd91db08206c5d438ba0776d81b16aa4dcd
illescas <illescas@srcorrecto.com>
2015-02-05 12:58:00 +0100

    tex

--- person.rb
+++ person.rb
@@ -1,8 +1,9 @@
 class Person
-  att_accesor :name, :surname
+  att_accesor :name, :surname, :age
 
-  def initialize(name, surname)
+  def initialize(name, surname, age)
     @name = name
     @surname = surname
+    @age = age
   end
 end
